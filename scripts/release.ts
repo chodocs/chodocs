88c4231509960ffdbffed7940aced534ec8acddd
Chocolate1999 <2816778271@qq.com>
2023-01-09 22:50:31 +0800

    chore: release v0.1.1

---
+++ scripts/release.ts
@@ -0,0 +1,20 @@
+import { execSync } from 'child_process'
+import { readJSONSync } from 'fs-extra'
+
+const { version: oldVersion } = readJSONSync('package.json')
+
+execSync('bumpp --no-commit --no-tag --no-push', { stdio: 'inherit' })
+
+const { version } = readJSONSync('package.json')
+
+if (oldVersion === version) {
+  console.log('canceled')
+  process.exit()
+}
+
+
+execSync('npm run update', { stdio: 'inherit' })
+execSync('git add .', { stdio: 'inherit' })
+
+execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' })
+execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
