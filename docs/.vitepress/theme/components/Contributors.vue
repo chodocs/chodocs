841ba7857c63df7a3f779ab8fdae0309ffccd2ff
Chocolate1999 <2816778271@qq.com>
2023-01-10 22:16:09 +0800

    feat: add contributors github link

--- docs/.vitepress/theme/components/Contributors.vue
+++ docs/.vitepress/theme/components/Contributors.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
 // @ts-expect-error missing types
-import _contributors from '/virtual-contributors'
-import { computed } from 'vue'
-import type { ContributorInfo } from '../../metadata'
+import _contributors from "/virtual-contributors";
+import { computed } from "vue";
+import type { ContributorInfo } from "../../metadata";
 
-const props = defineProps<{ doc: string }>()
+const props = defineProps<{ doc: string }>();
 
-const contributors = computed(() => _contributors[props.doc] || [] as ContributorInfo[]);
+const contributors = computed(
+  () => _contributors[props.doc] || ([] as ContributorInfo[])
+);
 
-const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
-
+const getAvatarUrl = (name: string) => `https://github.com/${name}.png`;
+const getGithubLink = (name: string) => `https://github.com/${name}`;
 </script>
 
 <template>
   <div class="flex flex-wrap gap-4 pt-2">
     <div v-for="c of contributors" :key="c.hash" class="flex gap-2 items-center">
-      <img :src="getAvatarUrl(c.name)" class="w-8 h-8 rounded-full">
+      <a :href="getGithubLink(c.name)" rel="noreferrer" target="_blank">
+        <img :src="getAvatarUrl(c.name)" class="w-8 h-8 rounded-full" />
+      </a>
       {{ c.name }}
     </div>
   </div>
