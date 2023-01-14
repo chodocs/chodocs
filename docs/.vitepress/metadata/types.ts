15a04ea8e501b722e55ea5510ef4f62cf3194374
Choi Yang <61035508+Chocolate1999@users.noreply.github.com>
2023-01-14 10:31:19 +0800

    feat: add changelog (#39)
    * feat: add changelog
    
    * fix: meta packages

--- docs/.vitepress/metadata/types.ts
+++ docs/.vitepress/metadata/types.ts
@@ -3,3 +3,15 @@ export interface ContributorInfo {
   count: number
   hash: string
 }
+
+export interface CommitInfo {
+  docs: string[]
+  version?: string
+  hash: string
+  date: string
+  message: string
+  refs?: string
+  body?: string
+  author_name: string
+  author_email: string
+}
---
+++ meta/packages.ts
@@ -0,0 +1,29 @@
+import type { PackageManifest } from "../types";
+
+
+export const packages: PackageManifest[] = [
+  {
+    name: 'algorithm',
+  },
+  {
+    name: 'essay',
+  },
+  {
+    name: 'green',
+  },
+  {
+    name: 'interview',
+  },
+  {
+    name: 'memo',
+  },
+  {
+    name: 'patterns',
+  },
+  {
+    name: 'tool',
+  },
+  {
+    name: 'ts',
+  },
+];
