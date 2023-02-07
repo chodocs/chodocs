da635b29b0913d15cd69a33eb6d0dbc18a6f5b9d
Choi Yang <2816778271@qq.com>
2023-02-07 15:11:31 +0800

    fix(theme): cut index.md of contributors (#103)

--- docs/.vitepress/plugins/markdownTransform.ts
+++ docs/.vitepress/plugins/markdownTransform.ts
@@ -10,6 +10,11 @@ export function MarkdownTransform(): Plugin {
         return null
       // convert links to relative
       code = code.replace(/https?:\/\/chodocs\.cn\//g, '/')
+      const [pkg, _name, i] = id.split('/').slice(-3)
+
+      // cut index.md
+      if (pkg === 'chodocs' && _name === 'docs' && i === 'index.md')
+        return code
 
       const { footer } = await getDocsMarkdown()
       code = replacer(code, footer, 'FOOTER', 'tail')
