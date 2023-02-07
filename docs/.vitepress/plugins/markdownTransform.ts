a1c1cf10c4f6b1f74433b10ba572ead6670b2cd2
Chocolate1999 <2816778271@qq.com>
2023-02-07 15:04:07 +0800

    fix(theme): cut index.md of contributors

--- docs/.vitepress/plugins/markdownTransform.ts
+++ docs/.vitepress/plugins/markdownTransform.ts
@@ -10,7 +10,12 @@ export function MarkdownTransform(): Plugin {
         return null
       // convert links to relative
       code = code.replace(/https?:\/\/chodocs\.cn\//g, '/')
+      const [pkg, _name, i] = id.split('/').slice(-3)
 
+      // cut index.md
+      if (pkg === 'chodocs' && _name === 'docs' && i === 'index.md') {
+        return code
+      }
       const { footer } = await getDocsMarkdown()
       code = replacer(code, footer, 'FOOTER', 'tail')
       return code
