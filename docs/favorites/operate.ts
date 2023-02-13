6ddada8fd4f63b5873bfd85f405d8a9a33759d3b
Chocolate1999 <2816778271@qq.com>
2023-02-13 21:24:25 +0800

    feat: add favorites page

--- docs/favorites/operate.ts
+++ docs/favorites/operate.ts
@@ -5,13 +5,13 @@ const operate: navItem[] = [
     id: 1,
     text: '百度统计',
     link: 'https://tongji.baidu.com/',
-    desc: 'One-stop intelligent data analysis and application platform'
+    desc: 'One-stop intelligent data analysis and application platform',
   },
   {
     id: 2,
     text: '百度 sitemap 收录',
     link: 'https://ziyuan.baidu.com/site/index#/',
-    desc: 'Website links submit'
+    desc: 'Website links submit',
   },
   {
     id: 3,
@@ -25,4 +25,4 @@ const operate: navItem[] = [
   },
 ]
 
-export default operate;
+export default operate
--- docs/favorites/regex.ts
+++ docs/favorites/regex.ts
@@ -5,20 +5,20 @@ const regex: navItem[] = [
     id: 1,
     text: 'visualRegex',
     link: 'https://wangwl.net/static/projects/visualRegex#',
-    desc: ''
+    desc: 'Visual regex',
   },
   {
     id: 2,
     text: 'regulex',
     link: 'https://jex.im/regulex/',
-    desc: ''
+    desc: 'JavaScript Regular Expression Visualizer',
   },
   {
     id: 3,
     text: 'RegExr',
     link: 'https://regexr.com/',
-    desc: ''
+    desc: 'Online tool to learn, build, & test Regular Expressions (RegEx / RegExp)',
   },
 ]
 
-export default regex;
+export default regex
