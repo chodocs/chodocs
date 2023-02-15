c4b11eb7c6f7d6979623dafa2dbb52eaf9df6f5a
Choi Yang <2816778271@qq.com>
2023-02-15 20:23:47 +0800

    feat(theme): add google analytics plugin (#141)

---
+++ docs/.vitepress/theme/plugins/google-analytics.ts
@@ -0,0 +1,28 @@
+declare const dataLayer: any[]
+declare const gtag: (...args: any[]) => void
+declare global {
+  interface Window {
+    dataLayer?: typeof dataLayer
+    gtag?: typeof gtag
+  }
+}
+
+const mountGoogleAnalytics = (id: string) => {
+  if (window.dataLayer && window.gtag)
+    return
+
+  const gtagScript = document.createElement('script')
+  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${id}`
+  gtagScript.async = true
+  document.head.appendChild(gtagScript)
+  window.dataLayer = window.dataLayer || []
+  window.gtag = function (...args) {
+    dataLayer.push(args)
+  }
+  gtag('js', new Date())
+  gtag('config', id)
+}
+export default ({ id }) => {
+  if (process.env.NODE_ENV === 'production' && id && typeof window !== 'undefined')
+    mountGoogleAnalytics(id)
+}
