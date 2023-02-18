8fb9207416310b6d3e98f353fa673c3d4a6b0a7b
Chocolate1999 <2816778271@qq.com>
2023-02-18 22:01:11 +0800

    style: mobile add px

--- docs/.vitepress/theme/components/DataPanel.vue
+++ docs/.vitepress/theme/components/DataPanel.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="panel mt-[48px]">
     <div class="mx-auto container max-w-[1152px] min-h-[32px] bg-[var(--vp-c-bg-soft)] w-full rounded-[8px]">
-      <section class="grid grid-cols-3 justify-items-center py-[12px]">
+      <section class="grid grid-cols-3 justify-items-center py-[12px] px-[24px] items-center">
         <h2 class="leading-[24px] text-[16px] font-[600]">
           本站总访问量 <span id="busuanzi_value_site_pv" class="font-bold" /> 次
         </h2>
