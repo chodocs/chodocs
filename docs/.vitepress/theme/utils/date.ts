2b26549851a6d183fa4fc4dfd9eba64d4d8da820
Choi Yang <2816778271@qq.com>
2023-02-24 11:27:48 +0800

    fix(utils): add dayjs zh-cn (#171)

--- docs/.vitepress/theme/utils/date.ts
+++ docs/.vitepress/theme/utils/date.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc.js'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn'
 dayjs.extend(utc)
 dayjs.locale('zh-cn')
 dayjs.extend(relativeTime)
