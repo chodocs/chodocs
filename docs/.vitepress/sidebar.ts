9f2a3d975cb77678a3848b21579a18305b68769a
Chocolate1999 <2816778271@qq.com>
2023-03-07 15:43:57 +0800

    feat(patterns): add middleware pattern

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -28,6 +28,7 @@ export default {
         { text: '观察者模式', link: '/patterns/observer-pattern/' },
         { text: '模块模式', link: '/patterns/module-pattern/' },
         { text: '混合模式', link: '/patterns/mixin-pattern/' },
+        { text: '中介/中间件模式', link: '/patterns/middleware-pattern/' },
       ],
     },
     {
