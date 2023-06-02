8e48cfe171e19c9297aaac172f755c29fd34753f
Choi Yang <2816778271@qq.com>
2023-06-02 22:53:18 +0800

    feat(theme): link enhance (#262)

---
+++ docs/.vitepress/theme/components/CustomLink.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { EXTERNAL_URL_RE, normalizeLink } from '../utils/md'
+
+interface ILink {
+  href: string
+  title: string
+  target?: string
+  rel?: string
+  tag?: string
+  desc?: string
+}
+
+const { href, title, tag = 'a', rel, target, desc } = defineProps<ILink>()
+
+const hrefSource = computed(() => {
+  if (/bilibili\.com/.test(href))
+    return 'bilibili'
+  if (/juejin\.cn/.test(href))
+    return 'juejin'
+  if (/mp\.weixin\.qq/.test(href))
+    return 'mpwx'
+  if (/weread\.qq\.com/.test(href))
+    return 'weread'
+  else if (/youtube\.com/.test(href))
+    return 'youtube'
+  else if (/github\.com/.test(href))
+    return 'github'
+  else if (/zsxq\.com/.test(href))
+    return 'zsxq'
+  else if (/vercel\.com/.test(href))
+    return 'vercel'
+  else if (/next[-]?js/.test(href))
+    return 'nextjs'
+  else return 'default'
+})
+
+const isExternal = computed(() => href && EXTERNAL_URL_RE.test(href))
+
+const component = computed(() => {
+  if (tag)
+    return tag
+
+  return href ? 'a' : 'button'
+})
+</script>
+
+<template>
+  <component
+    :is="component" :href="href ? normalizeLink(href) : undefined"
+    :target="target || (isExternal ? '_blank' : undefined)" :rel="rel || (isExternal ? 'noreferrer' : undefined)"
+    class="custom-link"
+  >
+    <section
+      class="flex group flex-col border-1 border-neutral-400 border-solid rounded-md p-4 gap-3 transition dark:border-neutral-600 duration-500 cursor-pointer my-4" :class="{
+        'hover:bg-zinc-300 dark:hover:bg-zinc-400': hrefSource === 'default',
+        'hover:bg-blue-300 dark:hover:bg-blue-400': hrefSource === 'bilibili' || hrefSource === 'juejin' || hrefSource === 'weread',
+        'hover:bg-rose-300 dark:hover:bg-red-400': hrefSource === 'youtube',
+        'hover:bg-slate-300 dark:hover:bg-slate-400': hrefSource === 'github' || hrefSource === 'vercel' || hrefSource === 'nextjs',
+        'hover:bg-emerald-300 dark:hover:bg-emerald-400': hrefSource === 'zsxq' || hrefSource === 'mpwx',
+      }"
+    >
+      <span
+        class="text-ellipsis w-[90%] whitespace-nowrap overflow-hidden dark:opacity-90 font-600 group-hover:text-[#06f]"
+      >{{
+        title }}</span>
+      <div v-if="desc" class="opacity-75 font-500 text-sm">
+        {{ desc
+        }}
+      </div>
+      <div class="flex item-center justify-between">
+        <div class="flex items-center gap-1 w-full">
+          <tabler:brand-bilibili v-if="hrefSource === 'bilibili'" class="text-blue-600 w-8 h-8 " />
+          <uiw:weixin v-if="hrefSource === 'weread'" class="text-blue-600 w-8 h-8 " />
+          <fe:youtube v-if="hrefSource === 'youtube'" class="text-red-600 w-8 h-8" />
+          <bi:github v-if="hrefSource === 'github'" class="text-slate-600 w-7 h-7" />
+          <ph:planet-fill v-if="hrefSource === 'zsxq'" class="text-emerald-600 w-8 h-8" />
+          <uiw:weixin v-if="hrefSource === 'mpwx'" class="text-emerald-600 w-8 h-8" />
+          <skill-icons:vercel-dark v-if="hrefSource === 'vercel'" class="text-zinc-800 w-7 h-7" />
+          <teenyicons:nextjs-solid v-if="hrefSource === 'nextjs'" class="text-zinc-800 w-7 h-7" />
+          <tabler:brand-juejin v-if="hrefSource === 'juejin'" class="text-blue-600 w-7 h-7" />
+          <span class="text-ellipsis w-full whitespace-nowrap overflow-hidden text-sm opacity-75 font-500">{{ href
+          }}</span>
+        </div>
+        <div class="items-center gap-1 hidden sm:flex">
+          <span
+            class="text-ellipsis w-full whitespace-nowrap overflow-hidden text-sm font-500 opacity-50"
+          >chodocs.cn</span>
+        </div>
+      </div>
+    </section>
+  </component>
+</template>
+
+<style scoped>
+.custom-link {
+  color: unset;
+  font-weight: normal;
+  text-decoration-style: none;
+}
+
+.custom-link:hover {
+  text-decoration: none;
+}
+</style>
--- docs/.vitepress/theme/utils/md.ts
+++ docs/.vitepress/theme/utils/md.ts
@@ -1,3 +1,5 @@
+import { useData, withBase } from 'vitepress'
+
 export function renderMarkdown(markdownText = '') {
   const htmlText = markdownText
     .replace(/^### (.*$)/gim, '<h3>$1</h3>')
@@ -17,4 +19,31 @@ export function renderMarkdown(markdownText = '') {
 export function renderCommitMessage(msg: string) {
   return renderMarkdown(msg)
     .replace(/\#([0-9]+)/g, '<a href=\'https://github.com/Chocolate1999/chodocs/issues/$1\'>#$1</a>')
+}
+export const EXTERNAL_URL_RE = /^[a-z]+:/i
+export const PATHNAME_PROTOCOL_RE = /^pathname:\/\//
+
+export function isExternal(path: string): boolean {
+  return EXTERNAL_URL_RE.test(path)
+}
+
+export function normalizeLink(url: string): string {
+  if (isExternal(url))
+    return url.replace(PATHNAME_PROTOCOL_RE, '')
+
+  const { site } = useData()
+  const { pathname, search, hash } = new URL(url, 'http://example.com')
+
+  const normalizedPath
+    = (pathname.endsWith('/') || pathname.endsWith('.html'))
+      ? url
+      : url.replace(
+        /(?:(^\.+)\/)?.*$/,
+        `$1${pathname.replace(
+          /(\.md)?$/,
+          site.value.cleanUrls ? '' : '.html',
+        )}${search}${hash}`,
+      )
+
+  return withBase(normalizedPath)
 }
