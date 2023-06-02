238f9c75d9b9f6a668128da99edd188949dde842
Chocolate1999 <2816778271@qq.com>
2023-06-02 15:11:55 +0800

    feat(weekly): link enhance and replace

--- docs/.vitepress/theme/components/CustomLink.vue
+++ docs/.vitepress/theme/components/CustomLink.vue
@@ -8,14 +8,32 @@ interface ILink {
   target?: string
   rel?: string
   tag?: string
+  desc?: string
 }
 
-const { href, title, tag = 'a', rel, target } = defineProps<ILink>()
+const { href, title, tag = 'a', rel, target, desc } = defineProps<ILink>()
 
-const isBilibili = /bilibili\.com/.test(href)
-const isYoutube = /youtube\.com/.test(href)
-const isGithub = /github\.com/.test(href)
-const isZSXQ = /zsxq\.com/.test(href)
+const hrefSource = computed(() => {
+  if (/bilibili\.com/.test(href))
+    return 'bilibili'
+  if (/juejin\.cn/.test(href))
+    return 'juejin'
+  if (/mp\.weixin\.qq/.test(href))
+    return 'mpwx'
+  if (/weread\.qq\.com/.test(href))
+    return 'weread'
+  else if (/youtube\.com/.test(href))
+    return 'youtube'
+  else if (/github\.com/.test(href))
+    return 'github'
+  else if (/zsxq\.com/.test(href))
+    return 'zsxq'
+  else if (/vercel\.com/.test(href))
+    return 'vercel'
+  else if (/next[-]?js/.test(href))
+    return 'nextjs'
+  else return 'default'
+})
 
 const isExternal = computed(() => href && EXTERNAL_URL_RE.test(href))
 
@@ -29,31 +47,42 @@ const component = computed(() => {
 
 <template>
   <component
-    :is="component" :href="href ? normalizeLink(href) : undefined" :target="target || (isExternal ? '_blank' : undefined)"
-    :rel="rel || (isExternal ? 'noreferrer' : undefined)" class="custom-link"
+    :is="component" :href="href ? normalizeLink(href) : undefined"
+    :target="target || (isExternal ? '_blank' : undefined)" :rel="rel || (isExternal ? 'noreferrer' : undefined)"
+    class="custom-link"
   >
     <section
-      class="flex flex-col border-1 border-neutral-400 border-solid rounded-md p-4 gap-4 transition  dark:border-neutral-600
-  cursor-pointer my-4" :class="{
-    'hover:bg-blue-300 dark:hover:bg-blue-400': isBilibili,
-    'hover:bg-rose-300 dark:hover:bg-red-400': isYoutube,
-    'hover:bg-slate-300 dark:hover:bg-slate-400': isGithub,
-    'hover:bg-emerald-300 dark:hover:bg-emerald-400': isZSXQ,
-      }" @click="() => {
-      }
-      "
+      class="flex group flex-col border-1 border-neutral-400 border-solid rounded-md p-4 gap-3 transition dark:border-neutral-600 duration-500 cursor-pointer my-4" :class="{
+        'hover:bg-zinc-300 dark:hover:bg-zinc-400': hrefSource === 'default',
+        'hover:bg-blue-300 dark:hover:bg-blue-400': hrefSource === 'bilibili' || hrefSource === 'juejin' || hrefSource === 'weread',
+        'hover:bg-rose-300 dark:hover:bg-red-400': hrefSource === 'youtube',
+        'hover:bg-slate-300 dark:hover:bg-slate-400': hrefSource === 'github' || hrefSource === 'vercel' || hrefSource === 'nextjs',
+        'hover:bg-emerald-300 dark:hover:bg-emerald-400': hrefSource === 'zsxq' || hrefSource === 'mpwx',
+      }"
     >
-      <span class="text-ellipsis w-[90%] whitespace-nowrap overflow-hidden dark:opacity-90 font-600">{{ title }}</span>
+      <span
+        class="text-ellipsis w-[90%] whitespace-nowrap overflow-hidden dark:opacity-90 font-600 group-hover:text-[#06f]"
+      >{{
+        title }}</span>
+      <div v-if="desc" class="opacity-75 font-500 text-sm">
+        {{ desc
+        }}
+      </div>
       <div class="flex item-center justify-between">
-        <div class="flex items-center gap-1">
-          <tabler:brand-bilibili v-if="isBilibili" class="text-blue-600 w-8 h-8 " />
-          <fe:youtube v-if="isYoutube" class="text-red-600 w-8 h-8" />
-          <bi:github v-if="isGithub" class="text-slate-600 w-8 h-8" />
-          <ph:planet-fill v-if="isZSXQ" class="text-emerald-600 w-8 h-8" />
+        <div class="flex items-center gap-1 w-full">
+          <tabler:brand-bilibili v-if="hrefSource === 'bilibili'" class="text-blue-600 w-8 h-8 " />
+          <uiw:weixin v-if="hrefSource === 'weread'" class="text-blue-600 w-8 h-8 " />
+          <fe:youtube v-if="hrefSource === 'youtube'" class="text-red-600 w-8 h-8" />
+          <bi:github v-if="hrefSource === 'github'" class="text-slate-600 w-7 h-7" />
+          <ph:planet-fill v-if="hrefSource === 'zsxq'" class="text-emerald-600 w-8 h-8" />
+          <uiw:weixin v-if="hrefSource === 'mpwx'" class="text-emerald-600 w-8 h-8" />
+          <skill-icons:vercel-dark v-if="hrefSource === 'vercel'" class="text-zinc-800 w-7 h-7" />
+          <teenyicons:nextjs-solid v-if="hrefSource === 'nextjs'" class="text-zinc-800 w-7 h-7" />
+          <tabler:brand-juejin v-if="hrefSource === 'juejin'" class="text-blue-600 w-7 h-7" />
           <span class="text-ellipsis w-full whitespace-nowrap overflow-hidden text-sm opacity-75 font-500">{{ href
           }}</span>
         </div>
-        <div class="flex items-center gap-1">
+        <div class="items-center gap-1 hidden sm:flex">
           <span
             class="text-ellipsis w-full whitespace-nowrap overflow-hidden text-sm font-500 opacity-50"
           >chodocs.cn</span>
@@ -69,7 +98,8 @@ const component = computed(() => {
   font-weight: normal;
   text-decoration-style: none;
 }
-.custom-link:hover{
+
+.custom-link:hover {
   text-decoration: none;
 }
 </style>
