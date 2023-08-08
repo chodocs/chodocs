b694ae5187ea6a43301feef7945a8f5b8edc3dc1
Chocolate1999 <2816778271@qq.com>
2023-08-08 09:53:17 +0800

    feat(theme): adjust dark mode style

--- docs/.vitepress/theme/components/CustomLink.vue
+++ docs/.vitepress/theme/components/CustomLink.vue
@@ -54,39 +54,50 @@ const component = computed(() => {
 </script>
 
 <template>
-  <component :is="component" v-if="isExternal" :href="href ? normalizeLink(href) : undefined" :target="target || (isExternal ? '_blank' : undefined)" :rel="rel || (isExternal ? 'noreferrer' : undefined)" class="custom-link">
-    <section class="flex group flex-col border-1 border-neutral-400 border-solid rounded-md p-4 gap-3 transition dark:border-neutral-600 duration-500 cursor-pointer my-4">
-      <span class="text-ellipsis w-[90%] whitespace-nowrap overflow-hidden dark:opacity-90 font-600 group-hover:text-[#06f]">{{ title }}</span>
-      <div v-if="desc" class="opacity-75 font-500 text-sm">
-        {{ desc
-        }}
-      </div>
-      <div class="flex item-center justify-between">
-        <div class="flex items-center gap-1 w-full max-w-[75%]">
-          <tabler:brand-bilibili v-if="hrefSource === 'bilibili'" class="text-blue-600 w-8 h-8 " />
-          <uiw:weixin v-if="hrefSource === 'weread'" class="text-blue-600 w-8 h-8 " />
-          <fe:youtube v-if="hrefSource === 'youtube'" class="text-red-600 w-8 h-8" />
-          <bi:github v-if="hrefSource === 'github'" class="text-slate-600 w-7 h-7" />
-          <ph:planet-fill v-if="hrefSource === 'zsxq'" class="text-emerald-600 w-8 h-8" />
-          <uiw:weixin v-if="hrefSource === 'mpwx'" class="text-emerald-600 w-8 h-8" />
-          <skill-icons:vercel-dark v-if="hrefSource === 'vercel'" class="text-zinc-800 w-7 h-7" />
-          <teenyicons:nextjs-solid v-if="hrefSource === 'nextjs'" class="text-zinc-800 w-7 h-7" />
-          <tabler:brand-juejin v-if="hrefSource === 'juejin'" class="text-blue-600 w-7 h-7" />
-          <skill-icons:typescript v-if="hrefSource === 'typescript'" class="w-7 h-7" />
-          <logos:react-query-icon v-if="hrefSource === 'react-query'" class="w-7 h-7" />
-          <skill-icons:react-dark v-if="hrefSource === 'react'" class="w-7 h-7" />
-          <skill-icons:stackoverflow-dark v-if="hrefSource === 'stackoverflow'" class="w-7 h-7" />
-          <icon-park:add-web v-if="hrefSource === 'web'" class="w-7 h-7" />
-          <span class="text-ellipsis w-full whitespace-nowrap overflow-hidden text-sm opacity-75 font-500">{{ href
-          }}</span>
-        </div>
-        <div class="items-center gap-1 hidden sm:flex">
+  <component
+    :is="component" v-if="isExternal" :href="href ? normalizeLink(href) : undefined"
+    :target="target || (isExternal ? '_blank' : undefined)" :rel="rel || (isExternal ? 'noreferrer' : undefined)"
+    class="custom-link"
+  >
+    <div class="custom-wrap rounded-lg p-px dark:shadow-lg shadow-black/20 my-4 border-zinc-300 border-solid border-px">
+      <div class="rounded-lg dark:bg-black">
+        <section class="flex group flex-col rounded-md p-4 gap-3 transition duration-500 cursor-pointer ">
           <span
-            class="text-ellipsis w-full whitespace-nowrap overflow-hidden text-sm font-500 opacity-50"
-          >chodocs.cn</span>
-        </div>
+            class="text-ellipsis w-[90%] whitespace-nowrap overflow-hidden dark:opacity-90 font-600 group-hover:text-[#06f]"
+          >{{
+            title }}</span>
+          <div v-if="desc" class="opacity-75 font-500 text-sm">
+            {{ desc
+            }}
+          </div>
+          <div class="flex item-center justify-between">
+            <div class="flex items-center gap-1 w-full max-w-[75%]">
+              <tabler:brand-bilibili v-if="hrefSource === 'bilibili'" class="text-blue-600 w-8 h-8 " />
+              <uiw:weixin v-if="hrefSource === 'weread'" class="text-blue-600 w-8 h-8 " />
+              <fe:youtube v-if="hrefSource === 'youtube'" class="text-red-600 w-8 h-8" />
+              <bi:github v-if="hrefSource === 'github'" class="text-slate-600 w-7 h-7" />
+              <ph:planet-fill v-if="hrefSource === 'zsxq'" class="text-emerald-600 w-8 h-8" />
+              <uiw:weixin v-if="hrefSource === 'mpwx'" class="text-emerald-600 w-8 h-8" />
+              <skill-icons:vercel-dark v-if="hrefSource === 'vercel'" class="text-zinc-800 w-7 h-7" />
+              <teenyicons:nextjs-solid v-if="hrefSource === 'nextjs'" class="text-zinc-800 w-7 h-7" />
+              <tabler:brand-juejin v-if="hrefSource === 'juejin'" class="text-blue-600 w-7 h-7" />
+              <skill-icons:typescript v-if="hrefSource === 'typescript'" class="w-7 h-7" />
+              <logos:react-query-icon v-if="hrefSource === 'react-query'" class="w-7 h-7" />
+              <skill-icons:react-dark v-if="hrefSource === 'react'" class="w-7 h-7" />
+              <skill-icons:stackoverflow-dark v-if="hrefSource === 'stackoverflow'" class="w-7 h-7" />
+              <icon-park:add-web v-if="hrefSource === 'web'" class="w-7 h-7" />
+              <span class="text-ellipsis w-full whitespace-nowrap overflow-hidden text-sm opacity-75 font-500">{{ href
+              }}</span>
+            </div>
+            <div class="items-center gap-1 hidden sm:flex">
+              <span
+                class="text-ellipsis w-full whitespace-nowrap overflow-hidden text-sm font-500 opacity-50"
+              >chodocs.cn</span>
+            </div>
+          </div>
+        </section>
       </div>
-    </section>
+    </div>
   </component>
   <component :is="component" v-else :href="href ? normalizeLink(href) : undefined" class="internal-link">
     {{ title }}
@@ -98,6 +109,15 @@ const component = computed(() => {
   color: unset;
   font-weight: normal;
   text-decoration-style: none;
+}
+
+.dark .custom-link {
+  color: white;
+}
+
+.dark .custom-wrap {
+  border: unset;
+  background-image: radial-gradient(at left top, #71717a, 50px, #27272a 50%);
 }
 
 .custom-link:hover {
