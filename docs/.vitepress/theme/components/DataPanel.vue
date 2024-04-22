d7c73eb19ee2c02f0d2939958da4aac409c89410
Chocolate1999 <ycychocolate@163.com>
2024-04-22 18:08:08 +0800

    style: update font size

--- docs/.vitepress/theme/components/DataPanel.vue
+++ docs/.vitepress/theme/components/DataPanel.vue
@@ -5,13 +5,13 @@
   <div class="panel mt-[48px]">
     <div class="mx-auto container max-w-[1152px] min-h-[32px] bg-[var(--vp-c-bg-soft)] w-full rounded-[8px]">
       <section class="grid grid-cols-3 justify-items-center py-[12px] px-[24px] items-center">
-        <h2 class="leading-[24px] text-[16px] font-[600]">
+        <span class="text-sm">
           本站总访问量 <span id="busuanzi_value_site_pv" class="font-bold" /> 次
-        </h2>
+        </span>
         <fa6-solid:heart-pulse class="heart" />
-        <h2 class="leading-[24px] text-[16px]">
+        <span class="text-sm">
           本站访客数 <span id="busuanzi_value_site_uv" class="font-bold" /> 人次
-        </h2>
+        </span>
       </section>
     </div>
   </div>
